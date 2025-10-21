5c621ebe56107234e179598e3a18c323c475bdc8
MelodyDuplaix <melo.surseine@gmail.com>
2025-10-21 11:33:19 +0200

    fix: correction de l'appel de template dans la route d'index et ajout de tests pour la prédiction

--- src/app/main.py
+++ src/app/main.py
@@ -35,7 +35,7 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(request, "index.html")
 
 
 if __name__ == "__main__":
