a834f169a3d9b6b91c36446f10c030b6fedb669b
MelodyDuplaix <melo.surseine@gmail.com>
2026-03-10 11:14:13 +0100

    feat: add mistral model

--- test.py
+++ test.py
@@ -31,3 +31,37 @@ db = SQLDatabase.from_uri(f"sqlite:///{local_path}")
 print(f"Dialect: {db.dialect}")
 print(f"Available tables: {db.get_usable_table_names()}")
 print(f'Sample output: {db.run("SELECT * FROM Artist LIMIT 5;")}')
+
+
+# passage à mistral en direct
+
+import os
+
+from mistralai import Mistral
+
+client = Mistral(api_key=os.environ.get("MISTRAL_API_KEY")) #clé api à rentrer dans le .env
+
+inputs = [
+        {
+            "role": "user",
+            "content": "Hello"
+        }
+    ]
+
+completion_args = {
+    "temperature": 0.7,
+    "max_tokens": 2048,
+    "top_p": 1
+}
+
+tools = []
+
+response = client.beta.conversations.start(
+    inputs=inputs,
+    model="mistral-medium-latest",
+    instructions="""""",
+    completion_args=completion_args,
+    tools=tools,
+)
+
+print(response)
